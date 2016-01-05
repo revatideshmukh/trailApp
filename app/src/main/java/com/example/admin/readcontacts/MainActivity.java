4d3171054b78023e2b19114cc7b6dfe599d2e2ee
Revati Deshmukh <revat@avantinsights.com>
2016-01-05 15:29:29 +0530

    for conflict

--- app/src/main/java/com/example/admin/readcontacts/MainActivity.java
+++ app/src/main/java/com/example/admin/readcontacts/MainActivity.java
@@ -98,29 +98,13 @@ public class MainActivity extends ActionBarActivity {
         cursor.close();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 
- /*   @Override
+
+
+    /*   @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -134,5 +118,13 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }*/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        System.out.println("hi in temp");
+        return true;
+    }
 
 }
