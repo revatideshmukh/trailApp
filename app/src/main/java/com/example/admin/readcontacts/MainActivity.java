c18f52d010a5403b225f63b0a340f54946718bd2
Revati Deshmukh <revat@avantinsights.com>
2016-01-05 11:09:48 +0530

    First Commit

---
+++ app/src/main/java/com/example/admin/readcontacts/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.admin.readcontacts;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableRow;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Button readcontacts;
+    TableRow number = null;
+    EditText numberRow = null;
+    private class InputValidator implements TextWatcher {
+
+        public void afterTextChanged(Editable s) {
+
+        }
+
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after) {
+
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before,
+                                  int count) {
+            if (numberRow.getText().toString().trim().length() != 0) {
+                if(numberRow.getText().toString().equals(".")){
+                    numberRow.setText("1.");
+                }
+                double value = Double.valueOf(numberRow.getText().toString());
+                if(value < 1 || value > 3.5){
+                    numberRow.setText("1");
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        readcontacts = (Button) findViewById(R.id.button);
+        number = (TableRow) MainActivity.this.findViewById(R.id.rownumber);
+        number.setVisibility(View.VISIBLE);
+        numberRow = (EditText) MainActivity.this.findViewById(R.id.editNumber);
+        numberRow.addTextChangedListener(new InputValidator());
+        readcontacts.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("In click listener");
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(R.string.dialog)
+                        .setPositiveButton(R.string.yes,
+                                new DialogInterface.OnClickListener() {
+
+                                    public void onClick(DialogInterface dialog,
+                                                        int which) {
+                                        System.out.println("again in click listener");
+                                        dialog.dismiss();
+                                        readContacts();
+                                    }
+                                })
+                        .setNegativeButton(R.string.no,
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int which) {
+                                        dialog.dismiss();
+                                    }
+                                }).create();
+                builder.show();
+            }
+        });
+    //   readContacts();
+    }
+
+    private void readContacts() {
+        // TODO Auto-generated method stub
+
+        Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null);
+
+        while (cursor.moveToNext()) {
+            String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            String name      = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+            System.out.println("Contact no "+contactId + " Contact Name " + name);
+        }
+        cursor.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
